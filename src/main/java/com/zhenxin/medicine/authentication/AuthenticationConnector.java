8a6a9493e430a197a0340b44e8478198fc1e1af8
Ivan <ivanpeng13@gmail.com>
2013-07-13 18:37:41 +0800

    Maven configurations and classpath modifications for functionality

--- src/main/java/com/zhenxin/medicine/authentication/AuthenticationConnector.java
+++ src/main/java/com/zhenxin/medicine/authentication/AuthenticationConnector.java
@@ -4,6 +4,12 @@ import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import net.glxn.qrgen.QRCode;
 import net.glxn.qrgen.image.ImageType;
@@ -17,7 +23,44 @@ import com.zhenxin.medicine.authentication.product.ProductItem;
 import com.zhenxin.medicine.authentication.product.ProductItemDaoImpl;
 import com.zhenxin.medicine.authentication.product.SerialGenerator;
 
-public class AuthenticationConnector {
+public class AuthenticationConnector extends HttpServlet{
+
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		PrintWriter out = resp.getWriter();
+		
+		// Execute the same command in main, but with request and response
+		ApplicationContext context = new ClassPathXmlApplicationContext(
+				"Database-Module.xml");
+		DrugItemDaoImpl dao = (DrugItemDaoImpl) context.getBean("drugDao");
+		DrugItem drugItem;
+		try{
+			drugItem = dao.getRefDataById(Integer.parseInt(req.getParameter("drugId")));
+		} catch(Exception ex)	{
+			drugItem = dao.getRefDataById(1);
+		}
+		// generate product now
+		ProductItem productItem = generateProduct(drugItem);
+		//TODO: add here, but need key generation set first
+		ProductItemDaoImpl daop = (ProductItemDaoImpl) context.getBean("productDao");
+		daop.addItem(productItem);
+		// Generate QR code here.
+		ByteArrayOutputStream qrOut = QRCode.from(productItem.getSerialCode()).to(ImageType.PNG).stream();
+		try	{
+			FileOutputStream fout = new FileOutputStream(new File("WebContent/images/QR_Test.PNG"));
+			fout.write(qrOut.toByteArray());
+			fout.flush();
+			fout.close();
+		} catch( IOException iex)	{
+			//TODO: close fout properly
+			out.println("Writing of QR code has failed.");
+		}
+		
+		// Now send the data to qrout.jsp
+		req.getRequestDispatcher("/qrout.jsp").forward(req, resp);
+	}
 
 	/**
 	 * Things to do: add in logging with log4j here.
